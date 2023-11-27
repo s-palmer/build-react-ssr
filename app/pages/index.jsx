888fd7caa8682c2af1947cee4fd6eee12318b54c
Sergei Palmer <10038681+s-palmer@users.noreply.github.com>
2023-11-27 10:32:33 +0000

    feat: set up fetch on the server and pass in data to the ssr index page

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
-export function HomePage() {
+export async function notNextServerSideProps(fetch) {
+  const data = await fetch("https://fakestoreapi.com/products").then((res) =>
+    res.json().then((json) => json)
+  );
+
+  return {
+    props: {
+      title: "All products",
+      products: data,
+    },
+  };
+}
+
+export function HomePage({ title }) {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>{title}</h1>
     </div>
   );
 }
--- server.jsx
+++ server.jsx
@@ -1,13 +1,16 @@
 import express from "express";
 import React from "react";
 import { renderToString } from "react-dom/server";
-import { HomePage } from "./app/pages/index.jsx";
+import { HomePage, notNextServerSideProps } from "./app/pages/index.jsx";
+const fetch = require("node-fetch");
 
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  const htmlContent = renderToString(<HomePage />);
+app.get("/", async (req, res) => {
+  const initialData = await notNextServerSideProps(fetch); 
+
+  const htmlContent = renderToString(<HomePage {...initialData.props} />);
 
   res.send(`
   <!DOCTYPE html>
